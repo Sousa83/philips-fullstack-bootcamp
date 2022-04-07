2bf11ce72aa26d3798ccdba8b31e1d64d538d62d
sousa83 <ricarosousa83@gmail.com>
2022-04-07 08:40:57 -0300

    angular-introduction: about components, interpolation and binding

--- angular/course-manager/src/app/app.component.ts
+++ angular/course-manager/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // nome da tag que irá referenciar esse componente
+  templateUrl: './app.component.html', // html do componente
+  styleUrls: ['./app.component.css'], // css do componente
 })
 export class AppComponent {
   title = 'course-manager';
+
+  nameOneBinding = 'SS83 1';
+  nameTwoBinding = 'SS83 2';
 }
