94e6195e68b4c830d7a8054880abee46633696e6
sousa83 <ricarosousa83@gmail.com>
2022-04-08 08:32:21 -0300

    angular-introduction: data binding events

--- angular/course-manager/src/app/coursers/course-list.component.ts
+++ angular/course-manager/src/app/coursers/course-list.component.ts
@@ -8,7 +8,10 @@ import { CourseService } from '../services/course.service';
   templateUrl: './course-list.component.html',
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
+  courses: Array<Course> = [];
+  filteredCourses: Array<Course> = [];
+
+  filterBy = '';
 
   // A injeção de dependências em componentes da-se pelos construtores
   constructor(private courseService: CourseService) {}
@@ -16,5 +19,19 @@ export class CourseListComponent implements OnInit {
   // A implementação de métodos de ciclo de vida da-se a implementação de interfaces
   ngOnInit(): void {
     this.courses = this.courseService.retriveAll();
+    this.filteredCourses = this.courses;
+  }
+
+  public set filter(value: string) {
+    this.filterBy = value;
+
+    const valueLowerCase = value.toLocaleLowerCase();
+    this.filteredCourses = this.courses.filter(
+      (c) => c.name.toLocaleLowerCase().indexOf(valueLowerCase) > -1
+    );
+  }
+
+  public get filter(): string {
+    return this.filterBy;
   }
 }
