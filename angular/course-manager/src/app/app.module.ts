d87c8085caae911a6f5895f8354aad63b2e03970
sousa83 <ricarosousa83@gmail.com>
2022-04-12 19:07:54 -0300

    angular-introduction: learning core angular module

--- angular/course-manager/src/app/app.module.ts
+++ angular/course-manager/src/app/app.module.ts
@@ -6,13 +6,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CourseModule } from './coursers/course.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { CoreModule } from './core/core.module';
 import { NotFoundComponent } from './notFound/not-found.component';
 
 @NgModule({
   // declarations é por onde importamos componentes, pipes, etc num módulo
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent],
   imports: [
+    CoreModule,
     CourseModule,
     BrowserModule,
     HttpClientModule,
