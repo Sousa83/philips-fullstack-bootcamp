1893e2e9e72638c90c74acb25afa31921255363c
sousa83 <ricarosousa83@gmail.com>
2022-04-18 08:46:19 -0300

    challange-components: button componente created

---
+++ angular/components-challenge/angular-components-challenge/src/app/shared/components/button/button.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './button.component';
+
+@NgModule({
+  exports: [ButtonComponent],
+  declarations: [ButtonComponent],
+})
+export class ButtonModule {}
