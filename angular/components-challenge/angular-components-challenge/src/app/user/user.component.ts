6e4800fdbc33f19c19105a628e3b1d8208f670e2
sousa83 <ricarosousa83@gmail.com>
2022-04-18 09:30:13 -0300

    challange-components: base implementation of table

--- angular/components-challenge/angular-components-challenge/src/app/user/user.component.ts
+++ angular/components-challenge/angular-components-challenge/src/app/user/user.component.ts
@@ -5,4 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {}
+export class UserComponent {
+  users = [
+    {
+      id: 1,
+      name: 'Ricardo',
+      email: 'ricarosousa83@gmail.com',
+      dioUrl: 'https://web.dio.me/users/ricarosousa83?tab=achievements',
+      imageUrl:
+        'https://hermes.digitalinnovation.one/users/student/8cd55ac9-72ad-4e50-80df-bf6dbdca6523.jpg',
+      role: 'developer',
+      departament: 'technology',
+    },
+  ];
+}
--- angular/components-challenge/angular-components-challenge/src/app/user/user.module.ts
+++ angular/components-challenge/angular-components-challenge/src/app/user/user.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from '../shared/components/button/button.module';
 
 import { UserComponent } from './user.component';
 
 @NgModule({
-  imports: [ButtonModule],
   exports: [UserComponent],
   declarations: [UserComponent],
+  imports: [ButtonModule, CommonModule],
 })
 export class UserModule {}
