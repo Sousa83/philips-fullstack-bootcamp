9f9e61c59024efc95f1c61556bf0dd21d396a484
sousa83 <ricarosousa83@gmail.com>
2022-04-18 08:27:17 -0300

    challange-components: user module created

--- angular/components-challenge/angular-components-challenge/src/app/app.module.ts
+++ angular/components-challenge/angular-components-challenge/src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UserModule],
 })
-export class AppModule { }
+export class AppModule {}
