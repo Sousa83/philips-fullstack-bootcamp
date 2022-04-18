d8a79f8068983137aa32406dd5ec7cdeb2dec44b
sousa83 <ricarosousa83@gmail.com>
2022-04-18 08:47:12 -0300

    challange-components: add user section implemented

--- angular/components-challenge/angular-components-challenge/src/app/user/user.module.ts
+++ angular/components-challenge/angular-components-challenge/src/app/user/user.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from '../shared/components/button/button.module';
+
 import { UserComponent } from './user.component';
 
 @NgModule({
+  imports: [ButtonModule],
   exports: [UserComponent],
   declarations: [UserComponent],
 })
