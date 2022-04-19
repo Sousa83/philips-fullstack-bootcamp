fc0ed5378a42051b060a721c2b61a81001764124
sousa83 <ricarosousa83@gmail.com>
2022-04-19 20:06:18 -0300

    challenge-components: listing page implemented

--- angular/components-challenge/angular-components-challenge/src/app/user/user.component.ts
+++ angular/components-challenge/angular-components-challenge/src/app/user/user.component.ts
@@ -11,6 +11,7 @@ export class UserComponent {
       id: 1,
       name: 'Ricardo',
       email: 'ricarosousa83@gmail.com',
+      addedOn: '2022-04-19T22:55:14.866Z',
       dioUrl: 'https://web.dio.me/users/ricarosousa83?tab=achievements',
       imageUrl:
         'https://hermes.digitalinnovation.one/users/student/8cd55ac9-72ad-4e50-80df-bf6dbdca6523.jpg',
--- angular/components-challenge/angular-components-challenge/src/app/user/user.module.ts
+++ angular/components-challenge/angular-components-challenge/src/app/user/user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ImageModule } from '../shared/components/image/image.module';
 import { ButtonModule } from '../shared/components/button/button.module';
 
 import { UserComponent } from './user.component';
@@ -8,6 +9,6 @@ import { UserComponent } from './user.component';
 @NgModule({
   exports: [UserComponent],
   declarations: [UserComponent],
-  imports: [ButtonModule, CommonModule],
+  imports: [ButtonModule, CommonModule, ImageModule],
 })
 export class UserModule {}
