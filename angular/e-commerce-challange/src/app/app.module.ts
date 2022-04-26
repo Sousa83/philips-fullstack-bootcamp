37841bdabf7fdd518ee1527ababfe8b347117896
sousa83 <ricarosousa83@gmail.com>
2022-04-26 08:03:36 -0300

    challenge-e-commerce: bootstrap configured

--- angular/e-commerce-challange/src/app/app.module.ts
+++ angular/e-commerce-challange/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavComponent } from './shared/components/nav/nav.component';
+import { BookstoreComponent } from './bookstore/bookstore.component';
+import { FilterComponent } from './bookstore/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookstoreComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
