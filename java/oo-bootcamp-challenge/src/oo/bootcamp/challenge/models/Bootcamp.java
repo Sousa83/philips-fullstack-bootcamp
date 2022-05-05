1b33a51a2f63dfa026eb4d2cada4602066c37994
sousa83 <ricarosousa83@gmail.com>
2022-05-05 19:58:42 -0300

    bootcamp-challenge: bootcamp and developer implemented

--- java/oo-bootcamp-challenge/src/oo/bootcamp/challenge/models/Bootcamp.java
+++ java/oo-bootcamp-challenge/src/oo/bootcamp/challenge/models/Bootcamp.java
@@ -1,4 +1,80 @@
 package oo.bootcamp.challenge.models;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Bootcamp {
+    private final LocalDate START_DATE = LocalDate.now();
+    private final LocalDate END_DATE = START_DATE.plusDays(120);
+    private final Set<Content> CONTENTS = new LinkedHashSet<>();
+
+    private String name;
+    private String description;
+    private Set<Developer> subscribedDevelopers = new HashSet<>();
+
+    public Bootcamp(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public LocalDate getSartDate() {
+        return START_DATE;
+    }
+
+    public LocalDate getEndDate() {
+        return END_DATE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Bootcamp setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Bootcamp setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Bootcamp { title: %s, desc %s }", name, description);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(START_DATE, bootcamp.START_DATE) && Objects.equals(END_DATE, bootcamp.END_DATE) && Objects.equals(CONTENTS, bootcamp.CONTENTS) && Objects.equals(name, bootcamp.name) && Objects.equals(description, bootcamp.description) && Objects.equals(subscribedDevelopers, bootcamp.subscribedDevelopers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(START_DATE, END_DATE, CONTENTS, name, description, subscribedDevelopers);
+    }
+
+    public Set<Developer> getSubscribedDevelopers() {
+        return subscribedDevelopers;
+    }
+
+    public Bootcamp setSubscribedDevelopers(Set<Developer> subscribedDevelopers) {
+        this.subscribedDevelopers = subscribedDevelopers;
+        return this;
+    }
+
+    public Set<Content> getContents() {
+        return CONTENTS;
+    }
+
 }
--- java/oo-bootcamp-challenge/src/oo/bootcamp/challenge/models/Developer.java
+++ java/oo-bootcamp-challenge/src/oo/bootcamp/challenge/models/Developer.java
@@ -1,4 +1,71 @@
 package oo.bootcamp.challenge.models;
 
-public class Developer {
+import oo.bootcamp.challenge.contracts.IExperience;
+import oo.bootcamp.challenge.enums.ContentStatus;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Developer implements IExperience {
+    private String name;
+    private Map<Content, ContentStatus> subscribedContent = new LinkedHashMap<>();
+
+    public Developer(String name) {
+        setName(name);
+    }
+
+    public void subscribeBootcamp(Bootcamp bootcamp) {
+        bootcamp.getContents().forEach(c -> subscribedContent.put(c, ContentStatus.UNFINISHED));
+        bootcamp.getSubscribedDevelopers().add(this);
+    }
+
+    public void finishContent() {
+        Optional<Map.Entry<Content, ContentStatus>> entryContent = subscribedContent
+                .entrySet()
+                .stream()
+                .filter(ec -> ec.getValue() != ContentStatus.FINISHED)
+                .findFirst();
+
+        if (entryContent.isPresent()) {
+            entryContent.get().setValue(ContentStatus.FINISHED);
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
+
+    @Override
+    public double calcExperience() {
+        return subscribedContent
+                .entrySet()
+                .stream()
+                .filter(ec -> ec.getValue() == ContentStatus.FINISHED)
+                .mapToDouble(ec -> ec.getKey().calcExperience())
+                .sum();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Developer: { name: %s, xp: %.2f }", getName(), calcExperience());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getSubscribedContent() {
+        return subscribedContent
+                .entrySet()
+                .stream()
+                .map(ec -> String.format("\n{ content: %s, status: %s }", ec.getKey().title, ec.getValue().toString()))
+                .collect(Collectors.joining());
+    }
+
+    public void setSubscribedContent(Map<Content, ContentStatus> subscribedContent) {
+        this.subscribedContent = subscribedContent;
+    }
 }
